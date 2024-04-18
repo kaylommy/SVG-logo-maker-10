07c3fc5ed65abb4ef30025511b8a2ed60687d15c
kaylommy <kaylajfreeman13@gmail.com>
2024-04-17 23:41:12 -0400

    fixed bug where triangle.svg would not generate

--- index.js
+++ index.js
@@ -50,18 +50,18 @@ inquirer
         if (answers.logoShape === 'circle'){
             const circle = new Circle(answers.logoText, answers.textColor, answers.shapeColor)
             fs.writeFile('./examples/circle.svg', circle.render(), error => {
-                // if there was an error it will be console logged, if successful it will log 'Generated logo.svg' in the console.
-                error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
+                // if there was an error it will be console logged, if successful it will log 'Generated ___.svg' in the console.
+                error ? console.log(error, 'Something went wrong') : console.log('Generated circle.svg');
             })
-        }else if (answers.logoShape === 'triange'){
+        }else if (answers.logoShape === 'triangle'){
             const triangle = new Triangle(answers.logoText, answers.textColor, answers.shapeColor)
             fs.writeFile('./examples/triangle.svg', triangle.render(), error => {
-                error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
+                error ? console.log(error, 'Something went wrong') : console.log('Generated triangle.svg');
             })
         }else if (answers.logoShape === 'square'){
             const square = new Square(answers.logoText, answers.textColor, answers.shapeColor)
             fs.writeFile('./examples/square.svg', square.render(), error => {
-                error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
+                error ? console.log(error, 'Something went wrong') : console.log('Generated square.svg');
             })
         }
 
