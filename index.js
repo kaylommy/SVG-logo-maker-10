2f2aa5c15331170c7207a39ef2d3c651d9912a79
kaylommy <kaylajfreeman13@gmail.com>
2024-04-17 22:38:18 -0400

    finished shapes.js file and connected it to the index added some if statements

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const inquirer = require('inquirer');
 // require fs for writing files (node file system)
 const fs = require('fs');
 // require the shapes.js file
-const { circle, triangle, square } = require('./lib/shapes');
-
-let shapeType;
-switch(shapeType){
-    case 'circle':
-        break;
-}
+const { Circle, Triangle, Square } = require('./lib/shapes');
 
 inquirer
     .prompt ([
@@ -36,7 +30,7 @@ inquirer
             type: 'list',
             message: 'Please choose a shape:',
             choices: ['circle', 'triangle', 'square'],
-            name: 'shapes',
+            name: 'logoShape',
         },
         {
             type: 'input',
@@ -44,8 +38,23 @@ inquirer
             name: 'shapeColor',
         },
     ]).then((answers) => {
-        fs.writeFile('./examples/logo.svg', JSON.stringify(answers, null), error => {
-            // if there was an error it will be console logged, if successful it will log 'Generated logo.svg' in the console.
-            error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
-        })
+        // if statement for each shape and their corresponding svg file
+        if (answers.logoShape === 'circle'){
+            const circle = new Circle(answers.logoText, answers.textColor, answers.shapeColor)
+            fs.writeFile('./examples/circle.svg', circle.render(), error => {
+                // if there was an error it will be console logged, if successful it will log 'Generated logo.svg' in the console.
+                error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
+            })
+        }else if (answers.logoShape === 'triange'){
+            const triangle = new Triangle(answers.logoText, answers.textColor, answers.shapeColor)
+            fs.writeFile('./examples/triangle.svg', triangle.render(), error => {
+                error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
+            })
+        }else if (answers.logoShape === 'square'){
+            const square = new Square(answers.logoText, answers.textColor, answers.shapeColor)
+            fs.writeFile('./examples/square.svg', square.render(), error => {
+                error ? console.log(error, 'Something went wrong') : console.log('Generated logo.svg');
+            })
+        }
+
     });
